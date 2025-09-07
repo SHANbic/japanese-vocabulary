3426ccb7ce1cc3d5ef0c871b6e002cfedbda4ec2
SHANbic <supagoof924@hotmail.com>
2025-09-07 16:01:05 +0200

    feat: add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ogImage from "./og-image.png";
 
 export const metadata: Metadata = {
   title: "Objectif Japon - Vocabulaire",
   description:
     "Testez votre maitrise du vocabulaire japonais pour chaque leçon d'Objectif Japon",
+  metadataBase: new URL("https://vocabulaire-objectif-japon.netlify.app/"),
+  openGraph: {
+    title: "Objectif Japon - Vocabulaire",
+    description:
+      "Testez votre maitrise du vocabulaire japonais pour chaque leçon d'Objectif Japon",
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Objectif Japon - Vocabulaire",
+    description:
+      "Testez votre maitrise du vocabulaire japonais pour chaque leçon d'Objectif Japon",
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
