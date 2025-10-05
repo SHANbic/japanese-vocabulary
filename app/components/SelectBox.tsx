b6ca23080d29e50495bde96930b9a2819dc63c2f
SHANbic <supagoof924@hotmail.com>
2025-10-05 12:34:16 +0200

    feat: add new lesson and create modules files

--- app/components/SelectBox.tsx
+++ app/components/SelectBox.tsx
@@ -2,45 +2,9 @@
 
 import { useState } from "react";
 import styles from "./box.module.css";
+import { modules } from "app/modules";
 
-const modules = [
-  {
-    label: "Module 1",
-    options: [
-      { value: "l1", text: "Leçon 1" },
-      { value: "l2", text: "Leçon 2" },
-      { value: "l3", text: "Leçon 3" },
-      { value: "l4", text: "Leçon 4" },
-      { value: "m1", text: "Toutes les leçons du module 1" },
-    ],
-  },
-  {
-    label: "Module 2",
-    options: [
-      { value: "l5", text: "Leçon 5" },
-      { value: "l6", text: "Leçon 6" },
-      { value: "l7", text: "Leçon 7" },
-      { value: "l8", text: "Leçon 8" },
-      { value: "m2", text: "Toutes les leçons du module 2" },
-    ],
-  },
-  {
-    label: "Module 3",
-    options: [
-      { value: "l9", text: "Leçon 9" },
-      { value: "l10", text: "Leçon 10" },
-      { value: "l11", text: "Leçon 11" },
-      { value: "l12", text: "Leçon 12" },
-      { value: "m3", text: "Toutes les leçons du module 3" },
-    ],
-  },
-  {
-    label: "Objectif Japon",
-    options: [{ value: "all", text: "Tout les modules!" }],
-  },
-];
-
-function Options() {
+const Options = () => {
   return modules.map((module) => (
     <optgroup key={module.label} label={module.label}>
       {module.options.map((option) => (
@@ -50,7 +14,7 @@ function Options() {
       ))}
     </optgroup>
   ));
-}
+};
 
 export default function SelectBox(props: {
   handleClick: (a: string, b: string) => void;
---
+++ app/modules.ts
@@ -0,0 +1,37 @@
+export const modules = [
+  {
+    label: "Module 1",
+    options: [
+      { value: "l1", text: "Leçon 1" },
+      { value: "l2", text: "Leçon 2" },
+      { value: "l3", text: "Leçon 3" },
+      { value: "l4", text: "Leçon 4" },
+      { value: "m1", text: "Toutes les leçons du module 1" },
+    ],
+  },
+  {
+    label: "Module 2",
+    options: [
+      { value: "l5", text: "Leçon 5" },
+      { value: "l6", text: "Leçon 6" },
+      { value: "l7", text: "Leçon 7" },
+      { value: "l8", text: "Leçon 8" },
+      { value: "m2", text: "Toutes les leçons du module 2" },
+    ],
+  },
+  {
+    label: "Module 3",
+    options: [
+      { value: "l9", text: "Leçon 9" },
+      { value: "l10", text: "Leçon 10" },
+      { value: "l11", text: "Leçon 11" },
+      { value: "l12", text: "Leçon 12" },
+      { value: "l13", text: "Leçon 13" },
+      { value: "m3", text: "Toutes les leçons du module 3" },
+    ],
+  },
+  {
+    label: "Objectif Japon",
+    options: [{ value: "all", text: "Tous les modules!" }],
+  },
+];
